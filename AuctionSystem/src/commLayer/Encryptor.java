0809b99248d55b581b0e8dcd88e1ad5bd05827d5
Novanet- <gamemaster8888@gmail.com>
2015-05-15 12:36:43 +0100

    Commented Encryptor file

--- AuctionSystem/src/commLayer/Encryptor.java
+++ AuctionSystem/src/commLayer/Encryptor.java
@@ -26,6 +26,19 @@ public class Encryptor
 	private static final String	transformation	= "AES";
 
 
+	/**
+	 * 
+	 * Converts a normal Output stream and returns an ObjectOutputStream that includes a CipherOutputStream which uses
+	 * AES to encrypt any data written to it
+	 * 
+	 * @param plainOutStream
+	 *            The normal ouput stream to be converted
+	 * @return The encrypting ObjectOutputStream
+	 * @throws IOException
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 * @throws InvalidKeyException
+	 */
 	public static ObjectOutputStream createEncryptedOutputStream(OutputStream plainOutStream) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException,
 			InvalidKeyException
 	{
@@ -43,6 +56,18 @@ public class Encryptor
 	}
 
 
+	/**
+	 * Converts a normal Input stream and returns an ObjectInputStream that includes a CipherInputStream which uses AES
+	 * to decrypt any data written to it
+	 * 
+	 * @param plainInStream
+	 *            The normal input stream to be converted
+	 * @return The decrypting ObjectInputStream
+	 * @throws IOException
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 * @throws InvalidKeyException
+	 */
 	public static ObjectInputStream createEncryptedInputStream(InputStream plainInStream) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException,
 			InvalidKeyException
 	{
@@ -56,6 +81,18 @@ public class Encryptor
 	}
 
 
+	/**
+	 * Writes a serializable message to an encrypting output stream
+	 * 
+	 * @param encryptingObjectPutStream
+	 *            An ObjectOutputStream that contains a CipherOutputStream allowing it to encrypt data
+	 * @param message
+	 *            The message to be encrypted and sent
+	 * @throws IOException
+	 * @throws InvalidKeyException
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 */
 	synchronized public static void writeToEncryptedStream(ObjectOutputStream objectOutStream, Serializable message) throws IOException, InvalidKeyException,
 			NoSuchAlgorithmException, NoSuchPaddingException
 	{
@@ -80,6 +117,17 @@ public class Encryptor
 	}
 
 
+	/**
+	 * Reads a serializable message to an decrypting input stream
+	 * 
+	 * @param plainInStream
+	 *            The decrypting input stream to be read from
+	 * @return The decrypted object that has been read
+	 * @throws IOException
+	 * @throws InvalidKeyException
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 */
 	synchronized public static Object readFromEncryptedStream(ObjectInputStream plainInStream) throws IOException, InvalidKeyException, NoSuchAlgorithmException,
 			NoSuchPaddingException
 	{
@@ -100,93 +148,5 @@ public class Encryptor
 			return null;
 		}
 	}
-
-
-	//	private static SealedObject sealMessage(Message message) throws IllegalBlockSizeException, IOException
-	//	{
-	//		SealedObject sealedObject = new SealedObject(message, cipher);
-	//		return sealedObject;
-	//	}
-	//
-	//
-	//	private static Message unsealMessage(SealedObject message) throws ClassNotFoundException, IllegalBlockSizeException, BadPaddingException, IOException
-	//	{
-	//		Message plainMessage = (Message) message.getObject(cipher);
-	//		return plainMessage;
-	//	}
-
-	private static Cipher initCipher()
-	{
-		try
-		{
-			return Cipher.getInstance("Blowfish");
-		}
-		catch (NoSuchAlgorithmException | NoSuchPaddingException e)
-		{
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	//	@Test
-	//	public void testEncrypt() {
-	//	  try {
-	//	    String s = "Hello there. How are you? Have a nice day.";
-	//
-	//	    // Generate key
-	//	    KeyGenerator kgen = KeyGenerator.getInstance("AES");
-	//	    kgen.init(128);
-	//	    SecretKey aesKey = kgen.generateKey();
-	//
-	//	    // Encrypt cipher
-	//	    Cipher encryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-	//	    encryptCipher.init(Cipher.ENCRYPT_MODE, aesKey);
-	//
-	//	    // Encrypt
-	//	    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-	//	    CipherOutputStream cipherOutputStream = new CipherOutputStream(outputStream, encryptCipher);
-	//	    cipherOutputStream.write(s.getBytes());
-	//	    cipherOutputStream.flush();
-	//	    cipherOutputStream.close();
-	//	    byte[] encryptedBytes = outputStream.toByteArray();
-	//
-	//	    // Decrypt cipher
-	//	    Cipher decryptCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-	//	    IvParameterSpec ivParameterSpec = new IvParameterSpec(aesKey.getEncoded());
-	//	    decryptCipher.init(Cipher.DECRYPT_MODE, aesKey, ivParameterSpec);
-	//
-	//	    // Decrypt
-	//	    outputStream = new ByteArrayOutputStream();
-	//	    ByteArrayInputStream inStream = new ByteArrayInputStream(encryptedBytes);
-	//	    CipherInputStream cipherInputStream = new CipherInputStream(inStream, decryptCipher);
-	//	    byte[] buf = new byte[1024];
-	//	    int bytesRead;
-	//	    while ((bytesRead = cipherInputStream.read(buf)) >= 0) {
-	//	        outputStream.write(buf, 0, bytesRead);
-	//	    }
-	//
-	//	    System.out.println("Result: " + new String(outputStream.toByteArray()));
-	//
-	//	  } catch (Exception ex) {
-	//	    ex.printStackTrace();
-	//	  }
-	//	}
-
-	//Looks to me like you are not dealing properly with your Initialization Vector (IV). It's been a long time since I last read about AES, IVs and block chaining, but your line
-	//
-	//IvParameterSpec ivParameterSpec = new IvParameterSpec(aesKey.getEncoded());
-	//
-	//does not seem to be OK. In the case of AES, you can think of the initialization vector as the "initial state" of a cipher instance, and this state is a bit of information that you can not get from your key but from the actual computation of the encrypting cipher. (One could argue that if the IV could be extracted from the key, then it would be of no use, as the key is already given to the cipher instance during its init phase).
-	//
-	//Therefore, you should get the IV as a byte[] from the cipher instance at the end of your encryption
-	//
-	//  cipherOutputStream.close();
-	//  byte[] iv = encryptCipher.getIV();
-	//
-	//and you should initialize your Cipher in DECRYPT_MODE with this byte[] :
-	//
-	//  IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
-	//
-	//Then, your decryption should be OK. Hope this helps.
 
 }
